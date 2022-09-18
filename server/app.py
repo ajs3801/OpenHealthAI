8a73a1b9e0bb366a9d9fa6627fe55111fbde0007
ajs3801 <tt14an@gmail.com>
2022-09-18 10:52:13 +0900

    implements ajax to send json file to python ML model

--- server/app.py
+++ server/app.py
@@ -1,57 +1,63 @@
+# import ML library
 import utils.Inference as I
 import utils.Preprocess as P
 
-from flask import Flask, request, render_template
+# for server
+from flask import Flask, request, render_template, jsonify
+import json
+
+# for ML
 import pandas as pd
 import pickle
 import warnings
-import json
 
 # Declare a Flask app
 app = Flask(__name__)
 
-# Main function here
-# ------------------
-@app.route('/', methods=['GET', 'POST'])
-def main():
-    # If a form is submitted
-    if request.method == "POST":
-      # Unpickle classifier
-      with open("../model/ActionV7.pkl", 'rb') as f:
-        model = pickle.load(f)
+# @app.route('/', methods=['GET', 'POST'])
+# def main():
+#     try:
+#       # If a form is submitted
+#       if request.method == "POST":
+#         # # Unpickle classifier
+#         # with open("../model/ActionV7.pkl", 'rb') as f:
+#         #   model = pickle.load(f)
 
-      with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        # Get values through input bars
-        coordinate_list = request.form.get("coordinate_list")
-        print(coordinate_list)
+#         # with warnings.catch_warnings():
+#         #   warnings.simplefilter("ignore")
+#         #   # Get values through input bars
+#         #   coordinate_list = request.form.get("coordinate_list")
+#         #   print(coordinate_list)
+#         coordinate_list = P.PreProcessToList(coordinate_list)
+#         current_class = I.Inference(coordinate_list)
 
-        coordinate_list = P.PreProcessToList(coordinate_list)
-        current_class = I.Inference(coordinate_list)
+#       else:
+#         current_class = "Input"
+      
+#       print('up')
 
-    else:
-      current_class = "Input"
-    
-    print('up')
-    return render_template("test.html", output = current_class)
+#       return render_template("test.html", output = current_class)
+#     except:
+#       print('SERVER ERROR')
 
-@app.route('/test', methods=['POST'])
-def test():
-  output = request.get_json()
-  result = json.loads(output) #this converts the json output to a python dictionary
+@app.route("/", methods = ["GET", "POST"])
+def connect():
+  print('connect')
+  if request.method == 'POST':
+    value_json = request.get_json()
+    value = value_json['data']
+    print('DATA - {}'.format(value))
+    print('DATA TYPE - {}'.format(type(value)))
 
-  coordinate_list = result['']
-  print(result)
+    value = P.PreProcessToList(value)
+    Inference_value = I.Inference(value)
+    print(Inference_value)
 
-  if request.method == "POST":
-    coordinate_list = P.PreProcessToList(coordinate_list)
-    current_class = I.Inference(coordinate_list)
-
+    return jsonify(result = "success", result2= Inference_value)
   else:
-    current_class = "Input"
+    return render_template('test.html')
 
-  print('down')
-  return render_template("test.html", output = current_class)
+
 # Running the app
 if __name__ == '__main__':
   app.run('0.0.0.0',port=8000,debug = True)
--- server/javascript/send.js
+++ server/javascript/send.js
@@ -1,19 +1,21 @@
-const coordinate_value = document.getElementById("coordinate_value").value;
 const Coordinate_btn = document.getElementById("Run");
 
-function onClick() {
-  const dict_values = {coordinate_value} //Pass the javascript variables to a dictionary.
-  const s = JSON.stringify(dict_values); // Stringify converts a JavaScript object or value to a JSON string
-
-  console.log(s); // Prints the variables to console window, which are in the JSON format
-  window.alert(s)
-  $.ajax({
-      url:"/test",
-      type:"POST",
-      contentType: "application/json",
-      data: JSON.stringify(s)});
-}
-
 Coordinate_btn.addEventListener('click', () => {
-  onClick();
-});
+  const coordinate_value = document.getElementById("coordinate_value").value;
+  console.log(coordinate_value)
+  const dict_values = {'data' : coordinate_value} //Pass the javascript variables to a dictionary.
+  const send = JSON.stringify(dict_values); // Stringify converts a JavaScript object or value to a JSON string
+  $.ajax({
+    type:"POST",
+    url : 'http://192.168.2.5:8000',
+    data: send,
+    dataType : 'JSON',
+    contentType: "application/json",
+    success : function(data){
+      alert('success');
+    },
+    error : function(request,status,error) {
+      alert("ERROR");
+    }
+  })
+})
